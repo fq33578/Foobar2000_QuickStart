504320592e8957ea34eef8aea946bb0ab511d133
CHEN,BO-HAN <fq33578@gmail.com>
2023-01-21 16:27:47 +0800

    Create Foobar2000_StartByAdmin.bat

---
+++ Foobar2000_StartByAdmin.bat
@@ -0,0 +1,22 @@
+::Start By Admin.
+@echo off
+REM --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+echo Requesting administrative privileges...
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+
+::Start Foobar By Realtime.
+path=%path%;C:\Program Files (x86)\foobar2000
+start /d"C:\Program Files (x86)\foobar2000" /realtime foobar2000.exe %1
